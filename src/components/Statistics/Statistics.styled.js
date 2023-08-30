262552ca6e81e0bf788951c1f669c76d453bc2d8
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-08-30 13:52:57 +0300

    statistics

---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,8 @@
+import { styled } from 'styled-components';
+
+export const StatisticField = styled.p`
+  border-radius: 4px;
+  padding: 4px;
+  width: 300px;
+  background-color: ${p => p.theme.colors.green};
+`;
