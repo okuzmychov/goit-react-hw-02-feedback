c39853032c1f2e78442143517e0820245503a8af
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-08-30 12:55:22 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { GlobalStyle } from 'GlobalStyle';
+import { Layout } from 'Layuot';
+import { Component } from 'react';
+import { FeedbackOptions } from './Buttons/Buttons';
+import { NoFeetback } from './NoFeetback/NoFeetback';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = feedbackType => {
+    this.setState(prevState => ({
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  render() {
+    const total = this.calculateTotal();
+    const positivePercentage = this.calculatePositivePercentage();
+    const noFeedback = this.calculateNoFeedback();
+
+    return (
+      <Layout>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {noFeedback ? (
+            <NoFeetback title="No ffetback given!" />
+          ) : (
+            <Statistics
+              options={this.state}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          )}
+        </Section>
+
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
