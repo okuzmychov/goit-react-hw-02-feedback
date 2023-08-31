d4fe980945e93728d82d61a8bfca11c30ae077f7
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-08-31 12:52:00 +0300

    style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,8 +4,8 @@ import 'modern-normalize';
 export const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  font-family: -apple-system, BlinkMacSystemFont, 'Ubuntu-Regular', 'Segoe UI', 'Oxygen', 'Roboto',
+    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
